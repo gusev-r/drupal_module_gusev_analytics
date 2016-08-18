5345e11052efba502e78c1a40eea8e93c421091e
gusev-r <gusevrv0o@gmail.com>
2016-08-18 17:24:00 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# drupal_module_gusev_analytics
---
+++ gusev-analytics-pages.tpl.php
@@ -0,0 +1,4 @@
+<?php foreach ($test as $key => $countPeople):?>
+    <strong><?php print $countPeople->name; ?></strong>
+    <?php print $countPeople->count; ?> <br>
+<?php endforeach; ?>
